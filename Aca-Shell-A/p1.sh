9f6cb53a353d47f4d79d932a6b6ed9ed7ba2a170
Mitsububunu <gav76873rilo@gmail.com>
2019-02-21 16:45:59 +0900

    解答

---
+++ Aca-Shell-A/p1.sh
@@ -0,0 +1,17 @@
+nc 2018shell.picoctf.com 27833
+ls
+cd secret
+ls
+rm intel*
+echo 'Drop it in!'
+cd ..
+cd executables
+ls
+./dontLookHere
+whoami
+cp /tmp/TopSecret passwords
+cd ..
+ls
+cd passwords
+ls
+cat TopSecret
